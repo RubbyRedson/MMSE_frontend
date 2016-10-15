3f21a15df07982bee0baa9da4844b57ee6d5f4e6
Victor Axelsson <victor_axelsson@hotmail.com>
2016-10-15 14:01:46 +0200

    Added tabs for the user stories in the dashboard

--- src/js/components/home/dashboard.js
+++ src/js/components/home/dashboard.js
@@ -22,9 +22,20 @@ class Dashboard extends Component {
     renderRows(){
       return (
           <div>
-            <DashboardItem name="Create application" index={0}/>
-            <DashboardItem name="Some other event" index={1}/>
-            <DashboardItem name="Yet another event" index={2}/>
+            <DashboardItem name="Create planning request (Customer service team)" index={0}/>
+            <DashboardItem name="Review planning requests (Customer service manager)" index={1}/>
+            <DashboardItem name="Review client request (Financial manager)" index={2}/>
+            <DashboardItem name="Review client request (Administration department)" index={3}/>
+            <DashboardItem name="Review client request (Customer service manager)" index={4}/>
+            <DashboardItem name="Create new client (Customer service manager)" index={5}/>
+            <DashboardItem name="Modify client (Customer service manager)" index={6}/>
+            <DashboardItem name="Give client discount (Customer service manager)" index={7}/>
+            <DashboardItem name="Give client discount (Customer service manager)" index={8}/>
+            <DashboardItem name="Send request to subteam (Department manager manager)" index={9}/>
+            <DashboardItem name="Fill expected plan(Subteam)" index={10}/>
+            <DashboardItem name="Request aditional resource (Department manager)" index={11}/>
+            <DashboardItem name="Create job advertisement (HR team)" index={12}/>
+            <DashboardItem name="Request budget negotiation (Deparment manager)" index={13}/>
           </div>
       ); 
     }
